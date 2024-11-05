a0ae9ae497e2853a5485949788d349c9c7cb17da
Manoela Urias <maanoelaurias@gmail.com>
2024-11-05 10:07:08 -0300

    refatoração do codigo

--- src/assets/shared/ComponenteTerminal/ComponenteTerminal.vue
+++ src/assets/shared/ComponenteTerminal/ComponenteTerminal.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
+import { defineProps } from 'vue';
 
+const props = defineProps<{
+  texto1: string; 
+  texto2: string;
+  texto3: string;
+}>();
 </script>
 
 <template>
     <div class="w-5/6 md:w-1/2">
         <h2 class="bg-gray-500 text-cyan-50 p-3 rounded-t">bash</h2>
         <div class="bg-black text-cyan-50 p-4 rounded-b">
-            <p class="p-1">Bem-vindo ao CommitChronicles, jovem aventureiro!</p>
-            <p class="p-1">Sua missão é dominar os comandos de Git enquanto explora o mundo de código. Enfrente
-                desafios, avance por
-                diferentes níveis e se torne um verdadeiro mestre das versões.</p>
-            <p class="p-1">Prepare-se para configurar sua identidade e registrar-se no reino do Git!</p>
+            <p class="p-1">{{ props.texto1 }}</p>
+            <p class="p-1">{{ props.texto2 }}</p>
+            <p class="p-1">{{ props.texto3 }}</p>
         </div>
     </div>
 </template>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -6,17 +6,24 @@ import { useRouter } from 'vue-router';
 const router = useRouter();
 
 function navigateTo() {
-  router.push({name: 'home'})
+  router.push({ name: 'home' })
 }
 </script>
 
 <template>
   <main class="w-screen h-screen flex items-center justify-center flex-col justify-evenly">
     <h1 class="text-3xl text-cyan-50 text-center">Bem-vindo ao CommitChronicles</h1>
-    <ComponenteTerminal/>
+    <ComponenteTerminal 
+      texto1="Bem-vindo ao CommitChronicles, aventureiro!"
+      texto2="Sua missão é dominar os comandos de Git enquanto explora o mundo de código. Enfrente
+                desafios, avance por
+                diferentes níveis e se torne um verdadeiro mestre das versões."
+      texto3="Prepare-se para configurar sua identidade e registrar-se no reino do Git!"
+       />
+
     <div class="flex gap-5 sm:flex-row">
-      <BotoesIniciais texto="Novo jogo" @click="navigateTo"/>
-      <BotoesIniciais texto="Continuar"/>
+      <BotoesIniciais texto="Nova jornada" @click="navigateTo" />
+      <BotoesIniciais texto="Continuar" />
     </div>
   </main>
 </template>
