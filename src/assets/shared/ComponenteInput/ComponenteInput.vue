ccf935167a4a09b82fb13a53b5cc8a30884cb410
Manoela Urias <maanoelaurias@gmail.com>
2024-11-05 13:15:27 -0300

    criação do componente de input de resposta

---
+++ src/assets/shared/ComponenteInput/ComponenteInput.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+    <div class="flex gap-4">
+        <!-- Input: Designation [h-12] & min-w-[12rem] -->
+        <input
+            class="h-12 w-full md:w-[25rem] rounded-lg border-emerald-500 indent-4 text-emerald-900 shadow-lg focus:outline-none focus:ring focus:ring-emerald-600"
+            type="text" placeholder="escreva o comando" />
+        <!-- Button: Submit [h-12] -->
+        <button
+            class="h-12 min-w-[8rem] rounded-lg border-2 border-emerald-600 bg-emerald-500 text-emerald-50 shadow-lg hover:bg-emerald-600 focus:outline-none focus:ring focus:ring-emerald-600">Enviar</button>
+    </div>
+
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import ComponenteInput from '@/assets/shared/ComponenteInput/ComponenteInput.vue';
 import ComponenteTerminal from '@/assets/shared/ComponenteTerminal/ComponenteTerminal.vue';
 
 
@@ -8,5 +9,7 @@ import ComponenteTerminal from '@/assets/shared/ComponenteTerminal/ComponenteTer
   <main class="w-screen h-screen flex items-center justify-center flex-col justify-evenly">
     <ComponenteTerminal bash="Primeira Missão: Configuração do Git" texto1="Para começar sua jornada, configure sua identidade com os comandos:
 " texto2="git config --global user.name 'Seu Nome'" texto3="git config --global user.email 'seuemail@exemplo.com'" />
+
+    <ComponenteInput />
   </main>
 </template>
