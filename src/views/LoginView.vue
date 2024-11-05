d783fcd608305478d0990138b9780f1cf115f7af
Manoela Urias <maanoelaurias@gmail.com>
2024-11-04 21:46:20 -0300

    estilização inicial

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-
+import BotoesIniciais from '@/assets/components/BotoesIniciais/BotoesIniciais.vue';
+import ComponenteTerminal from '@/assets/shared/ComponenteTerminal/ComponenteTerminal.vue';
 </script>
 
 <template>
-  <main>
-    <h1>Bem-vindo ao CommitChronicles</h1>
+  <main class="w-screen h-screen flex items-center justify-center flex-col">
+    <h1 class="text-3xl text-cyan-50">Bem-vindo ao CommitChronicles</h1>
+    <ComponenteTerminal/>
+    <div class="flex gap-5">
+      <BotoesIniciais/>
+      <BotoesIniciais/>
+    </div>
   </main>
 </template>
