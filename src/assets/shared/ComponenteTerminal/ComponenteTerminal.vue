2ecdcde8f0d613b4494a1546ee1cfe09bca09984
Manoela Urias <maanoelaurias@gmail.com>
2024-11-05 09:50:34 -0300

    Responsividade

--- src/assets/shared/ComponenteTerminal/ComponenteTerminal.vue
+++ src/assets/shared/ComponenteTerminal/ComponenteTerminal.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <div class="w-1/2 sm:w-[350px]">
+    <div class="w-5/6 md:w-1/2">
         <h2 class="bg-gray-500 text-cyan-50 p-3 rounded-t">bash</h2>
         <div class="bg-black text-cyan-50 p-4 rounded-b">
             <p class="p-1">Bem-vindo ao CommitChronicles, jovem aventureiro!</p>
