08a586db8a86174fc7b6b1c65b7111903ab5c40a
Manoela Urias <maanoelaurias@gmail.com>
2024-11-19 23:13:10 -0300

    .

--- src/assets/components/QuadroMissoes/QuadroMissoes.vue
+++ src/assets/components/QuadroMissoes/QuadroMissoes.vue
@@ -1,23 +1,16 @@
 <template>
     <main class="w-full h-screen">
-        <div class="bg-[url('../image/textura-madeira.jpg')] bg-cover  h-8 w-full rounded-lg z-2 flex justify-between align-center p-3 absolute top-0 left-0 flex">
-            <div class="bg-iron-500 rounded-full z-2 h-2 w-2"></div>
-            <div class="bg-iron-500 rounded-full z-2 h-2 w-2"></div>
-        </div>
+        <div
+            class="h-[75vh] w-[600px] rounded-md border-8 border-brown-800 flex justify-between items-center p-3 absolute top-20 left-0 ml-8 bg-brown-700 overflow-x-scroll overflow-y-hidden">
 
-        <div class="bg-[url('../image/textura-madeira.jpg')] bg-cover  h-8 w-full rounded-lg z-2 flex justify-between align-center p-3 absolute bottom-0 left-0 flex">
-            <div class="bg-iron-500 rounded-full z-2 h-2 w-2"></div>
-            <div class="bg-iron-500 rounded-full z-2 h-2 w-2"></div>
-        </div>
+            <div class="rolled-paper bg-gray-200 p-4 rounded-lg shadow-md">
+                <p>Seu conteúdo aqui</p>
+            </div>
 
-        <div class="bg-[url('../image/textura-madeira.jpg')] bg-cover  h-8 w-full rounded-lg z-2 flex justify-between align-center p-3 top-0 right-0">
-            <div class="bg-iron-500 rounded-full z-2 h-2 w-2"></div>
-            <div class="bg-iron-500 rounded-full z-2 h-2 w-2"></div>
-        </div>
-
-        <div class="bg-[url('../image/textura-madeira.jpg')] bg-cover h-8 w-full rounded-lg z-2 flex justify-between align-center p-3">
-            <div class="bg-iron-500 rounded-full z-2 h-2 w-2"></div>
-            <div class="bg-iron-500 rounded-full z-2 h-2 w-2"></div>
         </div>
     </main>
 </template>
+
+<style>
+
+</style>
