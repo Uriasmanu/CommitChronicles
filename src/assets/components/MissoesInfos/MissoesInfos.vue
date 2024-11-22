605f068011ae682095a5ef94ee89d7848756f239
Manoela Urias <maanoelaurias@gmail.com>
2024-11-21 21:24:04 -0300

    Implementação do modelo de missoes no quadro

--- src/assets/components/MissoesInfos/MissoesInfos.vue
+++ src/assets/components/MissoesInfos/MissoesInfos.vue
@@ -1,3 +1,8 @@
 <template>
-    
+  <div
+    class="max-h-44 w-52 bg-[url('@/image/pergaminho.png')] bg-cover bg-center flex justify-center flex-col items-center p-8 text-center gap-2"
+  >
+    <h2>titulo da tarefa</h2>
+    <p>pequena descricao da missao</p>
+  </div>
 </template>
--- src/assets/components/QuadroMissoes/QuadroMissoes.vue
+++ src/assets/components/QuadroMissoes/QuadroMissoes.vue
@@ -1,16 +1,15 @@
+<script setup lang="ts">
+import MissoesInfos from '../MissoesInfos/MissoesInfos.vue';
+</script>
+
 <template>
     <main class="w-full h-screen">
         <div
-            class="h-[75vh] w-[600px] rounded-md border-8 border-brown-800 flex justify-between items-center p-3 absolute top-20 left-0 ml-8 bg-brown-700 overflow-x-scroll overflow-y-hidden">
-
-            <div class="rolled-paper bg-gray-200 p-4 rounded-lg shadow-md">
-                <p>Seu conteúdo aqui</p>
-            </div>
+            class="h-[75vh] w-[600px] rounded-md border-8 border-brown-800 flex justify-between  p-3 absolute top-20 left-0 ml-8 bg-brown-700 overflow-x-scroll overflow-y-hidden">
+            <MissoesInfos />
 
         </div>
     </main>
 </template>
 
-<style>
-
-</style>
+<style></style>
