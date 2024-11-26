38280d1f41b820ef4f5380ddf3ba3219657dc467
Manoela Urias <maanoelaurias@gmail.com>
2024-11-25 21:34:40 -0300

    criação da função que ira receber os dados da api e exibir na tela

--- src/assets/components/MissoesInfos/MissoesInfos.vue
+++ src/assets/components/MissoesInfos/MissoesInfos.vue
@@ -2,7 +2,14 @@
   <div
     class="max-h-44 w-52 bg-[url('@/image/pergaminho.png')] bg-cover bg-center flex justify-center flex-col items-center p-8 text-center gap-2"
   >
-    <h2>titulo da tarefa</h2>
-    <p>pequena descricao da missao</p>
+    <h2>{{tituloDaTarefa}}</h2>
+    <p>{{pequenaDescricaoDaMissao}}</p>
   </div>
 </template>
+
+<script setup lang="ts">
+   defineProps({
+    tituloDaTarefa: String,
+    pequenaDescricaoDaMissao: String,
+   })
+</script>
--- src/assets/components/QuadroMissoes/QuadroMissoes.vue
+++ src/assets/components/QuadroMissoes/QuadroMissoes.vue
@@ -1,15 +1,44 @@
-<script setup lang="ts">
-import MissoesInfos from '../MissoesInfos/MissoesInfos.vue';
-</script>
-
 <template>
     <main class="w-full h-screen">
         <div
             class="h-[75vh] w-[600px] rounded-md border-8 border-brown-800 flex justify-between  p-3 absolute top-20 left-0 ml-8 bg-brown-700 overflow-x-scroll overflow-y-hidden">
-            <MissoesInfos />
+            <MissoesInfos
+                v-for="missao in missoes"
+                :key="missao.id"
+                :titulo-da-tarefa="missao.titulo"
+                :pequena-descricao-da-missao="missao.descricao"
+            />
 
         </div>
     </main>
 </template>
 
-<style></style>
+<script setup lang="ts">
+import axios from 'axios';
+import MissoesInfos from '../MissoesInfos/MissoesInfos.vue';
+import { ref, onMounted } from 'vue';
+
+interface Missao {
+  id: number;
+  titulo: string;
+  descricao: string;
+}
+
+
+// Cria uma referência para armazenar os dados das missões
+const missoes = ref<Missao[]>([]);
+
+// Função para buscar os dados da API
+const ListarMissoes = async () =>{
+    try {
+        const response = await axios.get('http://localhost:8081/api/Missoes');
+        missoes.value = response.data;
+        } catch (error) {
+            console.error(error);
+            
+    }
+};
+
+// Busca os dados quando o componente é montado
+onMounted(ListarMissoes);
+</script>
